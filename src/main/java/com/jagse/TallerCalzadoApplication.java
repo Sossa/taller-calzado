020384248ee94b164fd111c22e39affb0aff8925
Sossa <daniela.lopezsossa@gmail.com>
2026-04-14 20:46:17 -0500

    Cambios en pom y clase principal. Wow.

---
+++ src/main/java/com/jagse/TallerCalzadoApplication.java
@@ -0,0 +1,13 @@
+package com.jagse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerCalzadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerCalzadoApplication.class, args);
+	}
+
+}
